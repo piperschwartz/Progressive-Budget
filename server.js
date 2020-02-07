3f2aa60aa8377843e89d592f40436880574e4d62
piperschwartz <piperschwartz@aol.com>
2020-02-06 18:27:25 -0700

    fixing heroku

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 // routes here
 app.use(require("./routes/api.js"));
